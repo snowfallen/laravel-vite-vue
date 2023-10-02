78acdab06b606a8cf9a55560a7e1c1ba00776796
snowfallen <bondarenkoov1998@gmail.com>
2023-10-02 15:12:50 +0200

    fix pading

--- resources/js/src/Components/Login.vue
+++ resources/js/src/Components/Login.vue
@@ -2,14 +2,14 @@
     <div class="container">
         <form class="m-5">
             <div class="form-group pb-2">
-                <label for="email">Email address</label>
+                <label for="email" class="pb-2">Email address</label>
                 <input v-model="email" type="email" class="form-control" id="email" aria-describedby="emailHelp"
                        placeholder="Enter email">
                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
                     else.</small>
             </div>
             <div class="form-group pb-2">
-                <label for="password">Password</label>
+                <label for="password" class="pb-2">Password</label>
                 <input v-model="password" type="password" class="form-control" id="password" placeholder="Password">
             </div>
             <button @click.prevent="login" type="submit" class="btn btn-primary">Login</button>
