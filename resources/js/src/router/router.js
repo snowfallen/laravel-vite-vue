7076c037ec65d04b2530b3441b54ae94f7bf0f9e
snowfallen <bondarenkoov1998@gmail.com>
2023-10-02 16:35:56 +0200

    add basic route config

--- resources/js/src/router/router.js
+++ resources/js/src/router/router.js
@@ -6,4 +6,25 @@ const router = new createRouter({
     routes
 });
 
+router.beforeEach((to, from, next) => {
+    const token = localStorage.getItem('x-xsrf-token')
+
+    if (!token) {
+        if (to.name === 'user.login' || to.name === 'user.register') {
+            return next()
+        } else {
+            return next({
+                name: 'user.login'
+            })
+        }
+    }
+
+    if (to.name === 'user.login' || to.name === 'user.register' && token) {
+        return next({
+            name: 'pages.home'
+        })
+    }
+    next()
+})
+
 export default router;
